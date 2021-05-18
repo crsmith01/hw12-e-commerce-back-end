5eade10299658abf64e680230ff71610e8afb62b
Catherine Smith <catsmith24601@gmail.com>
2021-05-18 02:21:46 -0400

    added get, post, and delete requests to tag api routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,35 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      // JOIN with Product through ProductTag table
+      include: [{ model: Product, through: ProductTag, as: 'related_products' }]
+    });
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      // JOIN with Product through ProductTag table
+      include: [{ model: Product, through: ProductTag, as: 'related_products' }]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 router.post('/', (req, res) => {
@@ -23,6 +47,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 module.exports = router;
