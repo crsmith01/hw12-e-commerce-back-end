8893289d3f6d984b559d9ac7d3b9f4e3e10c9501
Catherine Smith <catsmith24601@gmail.com>
2021-05-18 09:48:40 -0400

    made api/tag-routes asynx await

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -54,8 +54,8 @@ router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
     const categoryData = await Category.update(
-      {category_name: req.body.category_name}, 
-      where {
+      {category_name: req.body.category_name},
+      where: {
         id: req.params.id
       }
     )};
