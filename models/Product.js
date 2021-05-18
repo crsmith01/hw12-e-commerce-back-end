bed9d5a316ea9278e14d598fc6d6d6cce64e217a
Catherine Smith <catsmith24601@gmail.com>
2021-05-18 01:03:18 -0400

    updated teh 4 models with specified requirements

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      // Specify (10,2) because 2 over for prices in decimal places
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validates: {
+        isDecimal: true
+      }
+    }, 
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validates: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
