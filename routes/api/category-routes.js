08cf1e6430582234e50294501d69b327a7af7553
Catherine Smith <catsmith24601@gmail.com>
2021-05-18 02:37:25 -0400

    updated post and put api routes for category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,11 +37,14 @@ router.get('/:id', async (req, res) => {
 
 });
 
+// PICK UP HERE
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const categoryData = await Category.create(req.body);
-    res.status(200).json(categoryData);
+    const categoryData = await Category.create({
+      id: req.body.id,
+      category_name: req.body.category_name,
+     } res.status(200).json(categoryData)
   } catch (Err) {
     res.status(400).json(err);
   }
@@ -51,9 +54,20 @@ router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
     const categoryData = await Category.update(
-      { }
-    )
+      {category_name: req.body.category_name}, 
+      where {
+        id: req.params.id
+      }
+    )};
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
   }
+  
 });
 
 router.delete('/:id', async (req, res) => {
